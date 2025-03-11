2927f4788f48defbea4eb924b7c4e6c7add72aaa
NabilKara <karanabil420@gmail.com>
2025-03-11 10:08:02 +0100

    adjusted a little bit on the menu

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,32 @@ mod menu;
 
 fn printMenu(){
     println!("PLease choose an option:");
-    println!("1- Cyphers");
+    println!("1- Ciphers");
     println!("2- Encryption Systems");
     println!("3- Help");
     println!("4- Quit");
 }
 
 fn main() {
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    loop {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 
-    let mut PATH = String::from("/");
-    printMenu();
-    let r = menu::getInput(PATH.clone(), 1, 4);
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-    match r {
-        1 => classical_ciphers::menu::Menu(&mut PATH),
-        _ => {},
+        let mut PATH = String::from("/");
+        printMenu();
+        let r = menu::getInput(PATH.clone(), 1, 4);
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        match r {
+            1 => {
+                classical_ciphers::menu::Menu(&mut PATH);
+                println!("------------------------------");
+            },
+            4 => {
+                println!("Good Bye !! ");
+                break;
+            },
+            _ => {
+                println!("This option isn't yet available");
+            }
+        }
     }
-
-    println!("Bye...");
 }
